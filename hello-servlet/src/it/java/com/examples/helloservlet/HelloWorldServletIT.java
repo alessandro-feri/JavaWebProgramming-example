57134fb53f61764b7e32ea2726ae4af0b604c76b
alessandro <alessandro.feri@stud.unifi.it>
2020-09-30 17:47:21 +0200

    Initial import

---
+++ hello-servlet/src/it/java/com/examples/helloservlet/HelloWorldServletIT.java
@@ -0,0 +1,46 @@
+package com.examples.helloservlet;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import java.util.logging.Logger;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+
+public class HelloWorldServletIT {
+private static final String BASE_URL = "/hello-servlet/HelloWorldServlet";
+
+		@BeforeClass
+		public static void setup() {
+			int tomcatPort =
+					Integer.parseInt(System.getProperty("tomcat.http.port", "8080"));
+			RestAssured.port = tomcatPort;
+			Logger.
+				getLogger(HelloWorldServletIT.class.toString()).
+				info("Using URL: " +
+						RestAssured.baseURI + ":" + RestAssured.port);
+			}
+
+	@Test
+	public void testGet() {
+		when().
+			get(BASE_URL).
+		then().
+			statusCode(200).
+			body(containsString("Served at: /hello-servlet"));
+}
+	@Test
+	public void testPost() {
+		given().
+				param("name", "TEST").
+		when().
+				post(BASE_URL).
+		then().
+				statusCode(200).
+				body(containsString("Hello,")).
+				body(containsString("TEST!"));
+	}
+	
+}
---
+++ hello-servlet/src/main/java/com/examples/helloservlet/HelloWorldServlet.java
@@ -0,0 +1,45 @@
+package com.examples.helloservlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Servlet implementation class HelloWorldServlet
+ */
+public class HelloWorldServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorldServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    @Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name = request.getParameter("name");
+		if (StringUtils.isEmpty(name))
+				name = "World";
+		request.setAttribute("user", name);
+		request.getRequestDispatcher("response.jsp")
+				.forward(request, response);
+	}
+
+
+}
